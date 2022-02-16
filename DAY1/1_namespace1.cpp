724a8d8bbc4948644735d227121aea34d901108b
ecourse <smkang@codenuri.co.kr>
2022-02-16 10:01:13 +0900

    Add files via upload

---
+++ DAY1/1_namespace1.cpp
@@ -0,0 +1,17 @@
+// 1_namespace1.cpp- 7page
+#include <stdio.h>
+
+void init()
+{
+	printf("init audio module\n");
+}
+
+void init()
+{
+	printf("init video module\n");
+}
+
+int main()
+{
+	init();
+}
