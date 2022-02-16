174a93a0b21f70c11eb0535d42143d34d75ff460
ecourse <smkang@codenuri.co.kr>
2022-02-16 11:02:30 +0900

    Add files via upload

---
+++ DAY1/2_표준입출력2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int n1, n2;
+	std::cin >> n1 >> n2; // 가능합니다. 그런데, 많이 사용하지는 않습니다.
+
+	n1 = 10;
+
+	std::cout << n1 << std::endl;
+	std::cout << n1 << std::endl;
+	std::cout << n1 << std::endl;
+
+}
