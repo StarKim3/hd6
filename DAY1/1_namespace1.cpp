efc431bcedb69804d52f1c00614ced5af834e84c
ecourse <smkang@codenuri.co.kr>
2022-02-16 10:07:03 +0900

    Add files via upload

--- DAY1/1_namespace1.cpp
+++ DAY1/1_namespace1.cpp
@@ -1,17 +1,24 @@
 // 1_namespace1.cpp- 7page
 #include <stdio.h>
-
-void init()
+// 1. 프로그램을 관련된 요소끼리 분리해서 관리할수 있다.
+// 2. 이름 충돌등을 막을수 있다.
+namespace Audio
 {
-	printf("init audio module\n");
+	void init()
+	{
+		printf("init audio module\n");
+	}
 }
-
-void init()
+namespace Video
 {
-	printf("init video module\n");
+	// Video 관련 함수, 구조체, 전역변수등을 모두 이안에 만든다.
+	void init()
+	{
+		printf("init video module\n");
+	}
 }
-
 int main()
 {
-	init();
+	Audio::init();
+	Video::init();
 }
