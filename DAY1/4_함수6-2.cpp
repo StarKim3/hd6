46ba37809e7ce627f292b19b5c2469026db45035
ecourse <smkang@codenuri.co.kr>
2022-02-16 16:21:13 +0900

    Add files via upload

--- DAY1/4_함수6-2.cpp
+++ DAY1/4_함수6-2.cpp
@@ -3,10 +3,19 @@
 
 // ? 채워 보세요
 template<typename T1, typename T2>
-? add(T1 a, T2 b)
-{
+//? add(T1 a, T2 b)				 // ? 를 채우기가 어려웠습니다
+//decltype(a + b)  add(T1 a, T2 b) // 그래서 decltype()을 만들었습니다. 그런데 왜 에러일까요 ?
+								 // a, b 변수를 선언전에 사용하는 코드 입니다.
+
+//auto add(T1 a, T2 b) -> decltype(a + b) // ok..이런 문제를 해결하기 위해 후위반환타입 문법생김
+										  // C++11 부터 가능.
+auto add(T1 a, T2 b) // C++14 부터 이렇게도 가능합니다
+{					 // return 문장을 보고 타입을 추론
 	return a + b;
 }
+
+
+
 
 int main()
 {
@@ -16,4 +25,7 @@ int main()
 	// 타입인자 생략가능
 	std::cout << add(1, 2.2) << std::endl;
 
+//	a = 20;    // error. 선언전에 사용
+//	int a = 0; // 변수 선언
+//	a = 10;    // ok.. 선언뒤 사용
 }
