12a8ee90530a003892123d6fb8a0d681582001d3
ecourse <smkang@codenuri.co.kr>
2022-02-16 13:12:02 +0900

    Add files via upload

--- DAY1/3_변수5.cpp
+++ DAY1/3_변수5.cpp
@@ -10,6 +10,16 @@ int main()
 	Point pt = { 1,2 };
 
 	// 구조체의 모든 요소를 한줄로 꺼낼수 있습니다
+	// C++17 에서 추가된 "structure binding" 이라는 문법 입니다 - 25page
+	// "g++ 소스.cpp -std=c++17" 필요 합니다
 	auto [a, b] = pt;	// auto a = pt.x
 						// auto b = pt.y 의 의미 입니다.
+
+	int[a1, b1] = pt; // error. auto만 가능합니다. !!
+
+
+	int arr[3] = { 1,2,3 };
+
+	auto [n1, n2, n3] = arr; // 배열도 됩니다.
+	auto [n4, n5] = arr; // error. 갯수가 동일해야만 합니다.
 }
