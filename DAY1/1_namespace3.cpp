4db370431c632212f5737e29705f69e60df9acf4
ecourse <smkang@codenuri.co.kr>
2022-02-16 10:24:48 +0900

    Add files via upload

--- DAY1/1_namespace3.cpp
+++ DAY1/1_namespace3.cpp
@@ -1,5 +1,5 @@
 #include <algorithm> // 이 헤더 안에 C++ 표준 "min" 함수가 있습니다.
-using namespace std; // 이렇게 하고 싶을때가 있습니다.
+//using namespace std; // 이렇게 하고 싶을때가 있습니다.
 					// 위험합니다. 실무에서는 이렇게 하지 마세요.
 
 // 모든 C++ 표준은 "std" 라는 이름공간에 있습니다
@@ -12,4 +12,7 @@ int main()
 	int n2 = std::min(10, 3); // ok
 
 	int n3 = count; // 왜, 여기서 에러가 나올까요 ?
+					// C++ 표준에 "std::count() 함수가 있습니다."
+					// using namespace std 때문에
+					// 전역변수 count 와 표준 함수 count 가 이름충돌발생
 }
