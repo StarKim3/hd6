392606bbf7df6675a3efdd256e2d4bda694d1f4a
ecourse <smkang@codenuri.co.kr>
2022-02-16 13:04:47 +0900

    Add files via upload

--- DAY1/3_변수4.cpp
+++ DAY1/3_변수4.cpp
@@ -1,10 +1,16 @@
-// 3_변수4
-typedef int DWORD;
-typedef void(*PF)();
+// 3_변수4 - 23page
+//typedef int DWORD;
+//typedef void(*PF)(); // 함수 포인터 타입
 
+// typedef : type에 대한 별칭 
+// using   : type에 대한 별칭 + template 에 대한 별칭
+
+// C++11 부터는 아래 처럼 해도 됩니다.
+using DWORD = int;
+using PF    = void(*)();
 
 int main()
 {
-	DWORD n;
-	PF    f;
+	DWORD n; // int n
+	PF    f; // 함수 포인터 변수
 }
