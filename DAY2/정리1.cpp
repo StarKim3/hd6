0273dc7019bfdc4c8f3873dac9c8d8508b8f3ed0
ecourse <smkang@codenuri.co.kr>
2022-02-17 11:16:53 +0900

    Add files via upload

---
+++ DAY2/정리1.cpp
@@ -0,0 +1,18 @@
+// 지금 까지 배운 것들
+// 1. C 보다 좋은 C++
+// 변수, 함수, 제어문 관련 C에는 없지만 C++에서 추가된 것들
+
+// 변수 : auto, decltype, using, namespace
+// 
+// 함수 : 디폴트 파라미터, overloading, inline, template
+//        함수삭제(=delete), 후위 반환 타입
+
+// 제어문 : range-for, 초기화 구문을 가진 if
+
+// reference 문법
+// malloc 대신 new 사용
+// 포인터 초기화에는 0대신 nullptr
+
+// 교재에 있는데 빠진것 : 캐스팅, constexpr
+// --------------------------
+// 2. 객체지향 프로그래밍
