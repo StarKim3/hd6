168c35ced794e2bc0ecee017dc39ad3f78b10cea
ecourse <smkang@codenuri.co.kr>
2022-02-17 16:01:57 +0900

    Add files via upload

---
+++ DAY2/6_생성자3.cpp
@@ -0,0 +1,16 @@
+// 6_»ı¼ºÀÚ3 - 78 page ~
+#include <iostream>
+
+class Point
+{
+	int x;
+	int y;
+public:
+	Point()				{ x=0; y=0; std::cout << "Point()" << std::endl; }
+	Point(int a, int b) { x=a; y=b; std::cout << "Point(int, int)" << std::endl; }
+};
+int main()
+{
+	Point p1;
+	Point p2(1, 2);
+}
