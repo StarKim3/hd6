e66e0c86f2a81fa509434d49c99a4a3dd92443f5
ecourse <smkang@codenuri.co.kr>
2022-02-17 10:51:44 +0900

    Add files via upload

--- DAY2/3_nullptr3.cpp
+++ DAY2/3_nullptr3.cpp
@@ -12,9 +12,16 @@ int main()
 	fn((void*)0); // (void*)0
 
 	// C언어에서 (void*)0 이 중요했습니다. 그래서 매크로로 만들었습니다.
-#define NULL (void*)0
-
+/*
+#ifdef __cplusplus			// 현재 컴파일러가 C++이라면
+	#define NULL 0
+#else 
+	#define NULL (void*)0
+#endif
+*/
 	fn(NULL); // void*
 
-	goo(NULL); // 생각해보세요.
+	goo(NULL); // void*  ==> char* 로의 암시적 형변환이 되어야 합니다
+				// C 언어 가능
+				// C++ 언어 안됨.
 }
