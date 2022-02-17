328d0b5a98f41204f63255b450e3e3c171a21819
ecourse <smkang@codenuri.co.kr>
2022-02-17 09:16:50 +0900

    Add files via upload

---
+++ DAY2/1_reference2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// 43 page ¿‘¥œ¥Ÿ.
+void inc1(int  n) { ++n; }
+void inc2(int* p) { ++(*p); }
+void inc3(int  r) { ++r; }
+
+int main()
+{
+	int a = 10, b = 10, c = 10;
+
+	inc1(a);
+	inc2(&b);
+	inc3(c ? &c);
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+}
