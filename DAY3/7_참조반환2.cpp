8f89095b311477d7a17528e7ca627633ef351349
ecourse <smkang@codenuri.co.kr>
2022-02-18 13:50:30 +0900

    Add files via upload

--- DAY3/7_참조반환2.cpp
+++ DAY3/7_참조반환2.cpp
@@ -5,12 +5,12 @@ class Counter
 public:
 	int cnt = 0;
 
-	Counter increment()
+//	Counter  increment()  // <<== 이렇게 만들면 main 처럼 사용시 버그 입니다. 임시객체생성
+	Counter& increment()  // <<== 이렇게 참조 반환 해야 합니다.
 	{
 		++cnt;
 		return *this;
 	}
-
 	~Counter()
 	{
 		std::cout << "~Counter()" << std::endl;
