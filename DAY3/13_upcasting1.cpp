4d7453a437ecfc0867f67650881e6a820c94a4ce
ecourse <smkang@codenuri.co.kr>
2022-02-18 16:27:50 +0900

    Add files via upload

--- DAY3/13_upcasting1.cpp
+++ DAY3/13_upcasting1.cpp
@@ -14,7 +14,19 @@ public:
 int main()
 {
 	Dog dog;
-	Dog*    p1 = &dog;
-	int*    p2 = &dog;
-	Animal* p3 = &dog;
+	Dog*    p1 = &dog; // ok
+//	int*    p2 = &dog; // error
+	Animal* p3 = &dog; // ok..
+
+	// 핵심 1. 기반 클래스 포인터로 파생 클래스 주소를 담을수 있다.(가리킬수 있다.)
+
+	// 핵심 2. 기반 클래스 포인터로는 기반 클래스 멤버만 접근가능하다
+	p3->cry();		 // ok.
+//	p3->setColor(3); // error.
+
+	// 핵심 3. Dog의 고유 멤버에 접근하려면
+	//			캐스팅한다.
+	((Dog*)p3)->setColor(3); // ok
+
+	static_cast<Dog*>(p3)->setColor(3); // 좋은 코드 - 월요일에
 }
