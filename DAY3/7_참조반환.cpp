c9369511ef5f1e1fcaf279f77c9df74c9b70e013
ecourse <smkang@codenuri.co.kr>
2022-02-18 13:37:05 +0900

    Add files via upload

--- DAY3/7_참조반환.cpp
+++ DAY3/7_참조반환.cpp
@@ -6,10 +6,13 @@ class Point
 public:
 	int x;
 	int y;
-};
 
-void f1(Point  p1) {}
-void f2(Point& p2) {}
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+// 핵심 1. 함수 호출과 객체 복사. 
+/*
+void f1(Point  p1) {} // call by value : 복사본 객체가 생성된다.
+void f2(Point& p2) {} // call by reference : 복사본을 만들지 않는다.
 
 int main()
 {
@@ -18,3 +21,24 @@ int main()
 	f1(p1);
 	f2(p2);
 }
+*/
+// 핵심 2. 함수 반환과 객체 복사
+Point pt = { 1,2 }; // 전역변수
+
+Point f3() // return by value 
+{
+	return pt; // 이순간 pt를 복사한 복사본 객체
+			   // (반환을 위한 임시객체) 가 생성되어서
+			   // 반환됩니다.
+			   // 리턴용 임시객체는 함수 호출 문장이 끝에서
+			   // 파괴 됩니다.
+}	
+
+int main()
+{
+	std::cout << "--------------" << std::endl;
+	f3();
+	std::cout << "--------------" << std::endl;
+}
+
+
