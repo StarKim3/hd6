7ab167244e67079b6c508514d029a7a0b0ea45f2
ecourse <smkang@codenuri.co.kr>
2022-02-18 15:42:51 +0900

    Add files via upload

--- DAY3/11_상속과생성자2.cpp
+++ DAY3/11_상속과생성자2.cpp
@@ -8,17 +8,16 @@ public:
 	Base(int a) { }
 	~Base()     { }
 };
-
 class Derived : public Base
 {
 public:
 	// 사용자가 만든 코드		// 컴파일러가 변경한 코드
-	Derived()      { }		//
-	Derived(int a) { }		//
-	~Derived()     { }		// 
+	Derived()      { }		// Derived()      : Base() { }
+	Derived(int a) { }		// Derived(int a) : Base() { }
+	~Derived()     { }		// ~Derived()     { ....; ~Base(); }
 };
 int main()
 {
-	//	Derived d1;
-	Derived d2(5);
+	Derived d1;    // call Derived::Derived()
+//	Derived d2(5); // call Derived::Derived(int)
 }
