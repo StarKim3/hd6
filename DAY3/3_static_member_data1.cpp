ad8ced761e0e59a5323bb2da5e96cec262871023
ecourse <smkang@codenuri.co.kr>
2022-02-18 09:37:24 +0900

    Add files via upload

--- DAY3/3_static_member_data1.cpp
+++ DAY3/3_static_member_data1.cpp
@@ -1,17 +1,25 @@
 // 3_static_member_data1 - 110 page
 #include <iostream>
 
+// 방법 1. 멤버 데이타 cnt 사용
+//		=> 멤버 데이타는 모든 객체에 각각 생성된다.
+//		=> 객체의 갯수 파악 실패!!
+
 class Car
 {
 	int color;
 public:
-	Car() {}
-	~Car() {}
+	int cnt = 0;
+
+	Car()  { ++cnt; }
+	~Car() { --cnt; }
 };
+
 int main()
 {
 	Car c1;
 	Car c2;
 
 	// 자동차 객체가 몇대나 생성되었는지 갯수를 파악하고 싶다.
+	std::cout << c1.cnt << std::endl;
 }
