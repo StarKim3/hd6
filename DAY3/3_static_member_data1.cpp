3d35b14532b1972f94fadba05569c5f535ab0152
ecourse <smkang@codenuri.co.kr>
2022-02-18 09:32:04 +0900

    Add files via upload

---
+++ DAY3/3_static_member_data1.cpp
@@ -0,0 +1,17 @@
+// 3_static_member_data1 - 110 page
+#include <iostream>
+
+class Car
+{
+	int color;
+public:
+	Car() {}
+	~Car() {}
+};
+int main()
+{
+	Car c1;
+	Car c2;
+
+	// 자동차 객체가 몇대나 생성되었는지 갯수를 파악하고 싶다.
+}
