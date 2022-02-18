180d2b7919b26ceefae5e4c9749196a249cb1f14
ecourse <smkang@codenuri.co.kr>
2022-02-18 13:10:06 +0900

    Add files via upload

--- DAY3/6_this1.cpp
+++ DAY3/6_this1.cpp
@@ -6,16 +6,23 @@ class Point
 	int x;
 	int y;
 public:
-	void set(int a, int b)
+	// 참고 : 파이썬 메소드 만들때   def __init__(self, a, b) 
+
+	void set(int a, int b) // void set(Point* this, int x, int y)
 	{
-		x = a;
-		y = b;
+		x = a; // this->x = a;  라는 의미로 변경됩니다.
+		y = b; // this->y = b;
 	}
 };
 int main()
 {
 	Point p1;
 	Point p2;
-	p1.set(10, 20);
-	p2.set(10, 20);
+	p1.set(10, 20); // set(&p1, 10, 20) 처럼 
+					// 객체주소도 함수로 전달합니다.
+
+	p2.set(10, 20);	// set(&p2, 10, 20)
+
+	// 주의 : 사용자가 직접 set(&p1, 10, 20) 처럼 할수는 없습니다.
+	// set(&p1, 10, 20); // error
 }
