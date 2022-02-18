5fa47537a55773836dd4bd98624250179a317b5c
ecourse <smkang@codenuri.co.kr>
2022-02-18 13:48:35 +0900

    Add files via upload

--- DAY3/7_참조반환2.cpp
+++ DAY3/7_참조반환2.cpp
@@ -4,16 +4,26 @@ class Counter
 {
 public:
 	int cnt = 0;
-	void increment()
+
+	Counter increment()
 	{
 		++cnt;
+		return *this;
+	}
+
+	~Counter()
+	{
+		std::cout << "~Counter()" << std::endl;
 	}
 };
 int main()
 {
 	Counter c;
-	c.increment();
-	c.increment();
+//	c.increment();
+//	c.increment();
+	std::cout << "----------------------" << std::endl;
+	c.increment().increment().increment(); 
+	std::cout << "----------------------" << std::endl;
 
-	std::cout << c.cnt << std::endl; 
+	std::cout << c.cnt << std::endl; // ???
 }
