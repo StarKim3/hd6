719c77bf0c6a54f2806f05be7c83a83d81c0615b
ecourse <smkang@codenuri.co.kr>
2022-02-18 14:36:53 +0900

    Add files via upload

--- DAY3/8_상수멤버함수.cpp
+++ DAY3/8_상수멤버함수.cpp
@@ -12,15 +12,26 @@ public:
 		x = a;
 		y = b;
 	}
-	void print()
+	// 상수 멤버 함수 : 멤버 함수 () 뒤에 const를 붙이는 문법
+	//				   해당 멤버함수에서는 "멤버데이타를 변경하지 않는다고"
+	//				   컴파일러에게 알려주는 것
+	void print() const 
 	{
+//		x = 10; // error
 		std::cout << x << ", " << y << std::endl;
 	}
 };
 int main()
 {
-	Point pt(1, 2);
-	pt.x = 10;
-	pt.set(10, 20);
-	pt.print();
+	const Point pt(1, 2);
+
+//	pt.x = 10;		// error. x는 public 에 있지만 상수 이므로
+//	pt.set(10, 20);	// error. 
+	pt.print();		// error. 
+					// 단, print 가 상수 멤버 함수 라면 호출 가능.
+
+	// "상수 객체는 상수 멤버함수 만 호출 할수 있다"
 }
+
+
+
