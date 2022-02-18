eb97209685970a8cb84c167df4f18b9bc880809e
ecourse <smkang@codenuri.co.kr>
2022-02-18 15:21:58 +0900

    Add files via upload

--- DAY3/9_상속.cpp
+++ DAY3/9_상속.cpp
@@ -2,17 +2,32 @@
 #include <iostream>
 #include <string>
 
-class Student
+// 상속(inheritance)
+// 기존 타입을 확장해서 새로운 타입을 설계하는 문법
+// C++    : class Student : public Person
+// C#     : class Student : Person
+// Python : class Student(Person)
+// java   : class Student extends Person
+
+// Person  : Base class(기반클래스), Super class, Parent class 
+// Student : Derived class(파생클래스), Sub class, Child class
+
+
+
+// 모든 사람의 공통의 특징을 먼저 타입으로 설계
+class Person				
 {
 	std::string name;
 	int age;
+};
+
+class Student : public Person
+{
 	int id;
 };
 
-class Professor
+class Professor : public Person
 {
-	std::string name;
-	int age;
 	int major;
 };
 
