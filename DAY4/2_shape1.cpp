432b0b0057bdc1cedea9ad13d865f6efe9812b4e
ecourse <smkang@codenuri.co.kr>
2022-02-21 09:48:15 +0900

    Add files via upload

---
+++ DAY4/2_shape1.cpp
@@ -0,0 +1,21 @@
+// 2_shape1.cpp  - 152 page
+#include <iostream>
+#include <vector>
+
+// 1. 모든 도형을 타입화 하면 편리합니다.
+
+class Rect
+{
+public:
+	void draw() { std::cout << "draw rect" << std::endl; }
+};
+class Circle
+{
+public:
+	void draw() { std::cout << "draw circle" << std::endl; }
+};
+
+int main()
+{
+
+}
