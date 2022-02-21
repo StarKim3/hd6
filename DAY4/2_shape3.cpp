0f224fb067e6a8a21bf35855e4114f4b986052f5
ecourse <smkang@codenuri.co.kr>
2022-02-21 10:24:05 +0900

    Add files via upload

--- DAY4/2_shape3.cpp
+++ DAY4/2_shape3.cpp
@@ -6,21 +6,30 @@
 //    그래야, 기반 클래스 포인터로 모든 도형을 묶어서 관리할때
 //    해당 기능을 사용할수 있다.
 
+// 4. 기반 클래스 함수중 파생 클래스가 재정의 하게 되는 함수는 
+//    반드시 가상함수로 설계 되어야 한다.
+
 class Shape
 {
+protected:
+	int color;
 public:
-	void draw() { std::cout << "draw shape" << std::endl; }
+	// 아래 함수는 virtual 로 할까요 ? 말까요 ?
+	// 파생 클래스가 재정의 할일을 거의 없습니다. virtual 로 할 필요 없습니다.
+	void setColor(int c) { color = c; }
+
+	virtual void draw() { std::cout << "draw shape" << std::endl; }
 };
 
 class Rect : public Shape
 {
 public:
-	void draw() { std::cout << "draw rect" << std::endl; }
+	virtual void draw() { std::cout << "draw rect" << std::endl; }
 };
 class Circle : public Shape
 {
 public:
-	void draw() { std::cout << "draw circle" << std::endl; }
+	virtual void draw() { std::cout << "draw circle" << std::endl; }
 };
 
 int main()
