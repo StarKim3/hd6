1eb29e9c8b4e877c4b630580ceebfe65c4b457ec
ecourse <smkang@codenuri.co.kr>
2022-02-22 15:38:06 +0900

    Add files via upload

--- DAY5/8_STL_ALGORITHM4.cpp
+++ DAY5/8_STL_ALGORITHM4.cpp
@@ -4,17 +4,22 @@
 #include <list>
 #include <algorithm>
 
+
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5, 1,2,3,4,5 };
 
 	// 주어진 구간에서 3을 0으로 변경해라
-	std::replace(v.begin(), v.end(), 3, 0);   // A
+//	std::replace(v.begin(), v.end(), 3, 0);   // A
 
+	// 짝수를 0으로 
+	std::replace_if(v.begin(), v.end(), [](int n) { return n % 2 == 0; }, 0);   // A
 
 	for (auto e : v)
 		std::cout << e << ", ";
 }
+
+
 // 실행해서 결과 확인해 보세요..
 // 확인하신 분은
 // "A" 를 주석으로 막고, 
