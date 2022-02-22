3a7cc7d6adaab6b4b8848be88c0686fb2c8584e6
ecourse <smkang@codenuri.co.kr>
2022-02-22 10:27:16 +0900

    Add files via upload

--- DAY5/3_cout4.cpp
+++ DAY5/3_cout4.cpp
@@ -16,10 +16,16 @@ std::ostream& operator<<(std::ostream& os, const Point& pt)
 	return os;
 }
 
-
 int main()
 {
 	Point p1(1, 1);
-
 	std::cout << p1; // operator<<(std::cout, p1 )
+
+//	std::ostream& os = std::cout;  // os 는 결국 cout 의 별칭입니다.
+//	os << 10; // os 는 cout 이므로 이렇게 사용하면 됩니다.
+
+//	const std::ostream& cos = std::cout; // 이렇게 하면 상수 별명입니다.
+//	cos << 10; // error 입니다. 상수객체는 상수 함수만 호출가능한데
+			   // cos.operator<<(10) 에서 "operator<<" 는 상수 멤버함수 아닙니다.
+
 }
