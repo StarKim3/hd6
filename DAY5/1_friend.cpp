6da439eafc59c5e61cf9f464d085eb782a43a93f
ecourse <smkang@codenuri.co.kr>
2022-02-22 09:04:36 +0900

    Add files via upload

--- DAY5/1_friend.cpp
+++ DAY5/1_friend.cpp
@@ -4,6 +4,10 @@ class Bike
 {
 	int gear;
 public:
+
+	// friend 함수 : 멤버 함수는 아니지만 private 멤버에 접근할수 있게 
+	//				 해달라는 의미.
+	friend void foo();
 };
 
 void foo()
@@ -11,6 +15,7 @@ void foo()
 	Bike b;
 	b.gear = 10;
 }
+
 int main()
 {
 	foo();
