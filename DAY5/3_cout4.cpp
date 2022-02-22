a2ca2ec69daeb98f56db7f34b7369235caa9999c
ecourse <smkang@codenuri.co.kr>
2022-02-22 10:19:19 +0900

    Add files via upload

---
+++ DAY5/3_cout4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Point
+{
+	int x;
+	int y;
+public:
+	Point(int a, int b) : x(a), y(b) {}
+};
+// 핵심 : 사용자 정의 타입을 cout 으로 출력하려면 아래 같은 함수를 만들면 됩니다.
+std::ostream& operator<<(std::ostream& os, Point& pt)
+{
+	os << pt.x << ", " << pt.y;
+	return os;
+}
+
+
+int main()
+{
+	Point p1(1, 1);
+
+	std::cout << p1; 
+}
