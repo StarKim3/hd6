73ea60d6fdba76034b86d04d7aaa613edd72f79e
ecourse <smkang@codenuri.co.kr>
2022-02-22 16:37:14 +0900

    Add files via upload

--- DAY5/10_캐스팅3.cpp
+++ DAY5/10_캐스팅3.cpp
@@ -6,12 +6,26 @@
 
 // 그래서 C++에서는 용도별로 캐스팅연산자를 제공합니다.
 
+// static_cast      : 위험하지 않고, 논리적으로 필요한 것만 허용
+// reinterpret_cast : 다른 타입의 주소를 캐스팅할때 사용
 int main()
 {
-	int* p1 = (int*)malloc(20);				 // C 스타일
-	int* p2 = static_cast<int*>( malloc(20) );// C++스타일
+	// void* => int* : 반드시 필요한 작업이다.
+	int* p1 = (int*)malloc(20);				 // C 스타일   - ok
+	int* p2 = static_cast<int*>( malloc(20) );// C++스타일 - ok
 
+	// int* => double* : 아주 위험하다.!
 	int n = 3;
-	double* p3 = (double*)&n;
-	double* p4 = static_cast<double*>(&n);
+	double* p3 = (double*)&n;	// ok
+//	double* p4 = static_cast<double*>(&n); // error
+	double* p4 = reinterpret_cast<double*>(&n); // ok.
+
+	const int c = 10;
+	int* p5 = (int*)&c;              // ok. 하지만 너무 위험하다.
+	int* p6 = static_cast<int*>(&c); // error
+	int* p7 = reinterpret_cast<int*>(&c); // error
+	int* p8 = const_cast<int*>(&c); // ok. 
+
+	// 즉, C++은 용도별로 다른 형태로 캐스팅해서.
+	// 의도를 명확히 코드에 표현한다.
 }
