18ca12e9a835c662378197cfc67e25b4eac9a6ce
ecourse <smkang@codenuri.co.kr>
2022-02-22 14:09:49 +0900

    Add files via upload

--- DAY5/7_STL_ITERATOR1.cpp
+++ DAY5/7_STL_ITERATOR1.cpp
@@ -1,6 +1,7 @@
 // 7_STL_ITERATOR1
 #include <iostream>
 #include <list>
+#include <vector>
 
 int main()
 {
@@ -10,10 +11,17 @@ int main()
 	int* p1 = x;
 	++p1;
 
-	std::list<int> s = { 1,2,3,4,5 };
+//	std::list<int> s = { 1,2,3,4,5 };
+	std::vector<int> s = { 1,2,3,4,5 };
 
 	// STL 은 반복자(iterator)를 꺼내면 모든 요소를 열거할수 있습니다
 	auto p2 = s.begin();
-	++p2;
+	
+	++p2; // p2.operator++() 호출됩니다
+	
 	std::cout << *p2 << std::endl;
+			// p2.operator*()
+			// 즉, p2는 진짜 포인터는 아니지만
+			// 포인터 처럼 사용가능하게
+			// 연산자 재정의 되어 있습니다.
 }
