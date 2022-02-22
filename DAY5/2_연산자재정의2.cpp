eb8e9be69c478c9c90acb89c35ee385f2a6cb8fd
ecourse <smkang@codenuri.co.kr>
2022-02-22 09:27:41 +0900

    Add files via upload

--- DAY5/2_연산자재정의2.cpp
+++ DAY5/2_연산자재정의2.cpp
@@ -9,6 +9,7 @@ public:
 	Point(int a, int b) : x(a), y(b) {}
 
 	// 멤버로 구현한 "operator+" 라는 이름의 함수
+	// + 는 이항 연산자 이지만, 멤버 함수로 만들면 인자는 "한개" 입니다
 	Point operator+(const Point& pt)
 	{
 		std::cout << "member operator+" << std::endl;
@@ -16,7 +17,6 @@ public:
 		return temp;
 	}
 };
-
 int main()
 {
 	Point p1(1, 1);
@@ -25,5 +25,7 @@ int main()
 	Point p3 = p1 + p2;  // 1. p1.operator+(p2) 가 가능한가 ?
 						 // 2. operator+(p1, p2) 가 가능한가 ?
 						 // 모두 안되면 error
+	Point p4 = p1.operator+(p2); // 이렇게 해도 됩니다.
+								 // "operator+" 도 결국 일반적인 멤버 함수 입니다.
 
 }
