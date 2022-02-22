e83e67fec6e07bc35aeff7ed5fd5d016c6116cd1
ecourse <smkang@codenuri.co.kr>
2022-02-22 13:02:13 +0900

    Add files via upload

---
+++ DAY5/6_STL_CONTAINER1.cpp
@@ -0,0 +1,11 @@
+// 6_STL_CONTAINER1 - 214p ~ 
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main()
+{
+	// C ¹è¿­ vs STL vector
+	int x[5] = { 1,2,3,4,5 };
+	std::vector<int> v = { 1,2,3,4,5 };
+}
