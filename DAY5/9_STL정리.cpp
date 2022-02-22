bc5662881e1cf45478c67224a684e57c2573ce5d
ecourse <smkang@codenuri.co.kr>
2022-02-22 15:42:34 +0900

    Add files via upload

---
+++ DAY5/9_STL정리.cpp
@@ -0,0 +1,13 @@
+// 9_STL정리
+// STL 3대 요소 : container, iterator, algorithm
+
+// 1. 작성하는 업무에 가장 적합한 컨테이너를 고르세요(자료구조 학습)
+
+// 2. 컨테이너에 저장된 데이타에 어떤 연산을 하고자 한다면
+//    직접 만들지 말고, STL 알고리즘에서 찾으세요
+//    => 모든 요소의 합을 구하고자 한다면  "std::accumulate"
+//    => 모든 요소의 뒤집으려면  "std::reverse"
+//    => 최대 값을 구하려면      "std::max"
+//    => 이진 검색 하려면        "std::binary_search"
+
+// cppreference.com 에 접속후 1번째 화면에서 "Algorithm Library" 찾으세요.
