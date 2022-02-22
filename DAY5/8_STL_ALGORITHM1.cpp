56bd4ddafa3cda6a21588839fe3dfb23bdee77ec
ecourse <smkang@codenuri.co.kr>
2022-02-22 15:12:39 +0900

    Add files via upload

--- DAY5/8_STL_ALGORITHM1.cpp
+++ DAY5/8_STL_ALGORITHM1.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <list>
-#include <algorithm>
+#include <algorithm> // find 같은 일반화 함수(템플릿)
+
+// 알고리즘 : 문제를 해결하는 방법 이라는 일반적인 용어도 있지만
+//           STL 에서 "find" 같은 멤버가 아닌 일반 함수를 "알고리즘"이라고 합니다.
+
 
 int main()
 {
@@ -15,10 +19,12 @@ int main()
 
 	// STL 의 find 는 멤버가 아닌 일반 함수(템플릿)으로 되어 있습니다
 	// 하나의 find 가 모든 컨테이너에 대해서 "선형검색"을 수행하도록 되어 있습니다.
+	
+	// std::find(first, last, 값) 에서 [first, last) 검색 입니다. "half-open구간"
 	auto ret1 = std::find(s.begin(), s.end(), 3);
 	auto ret2 = std::find(v.begin(), v.end(), 3);
 
-	// 결과(ret2)
+	// 결과(ret2) 는 "반복자(iterator)" 입니다.
 	// 검색 실패시 : v.end() 가 반환 됩니다.
 	// 검색 성공시 : 3이 있는 곳을 가리키는 반복자 반환
 	if (ret2 == v.end())
