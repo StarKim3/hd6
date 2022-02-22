740ebfc4a03b9b171a2f20fae3223380303e6b5f
ecourse <smkang@codenuri.co.kr>
2022-02-22 09:11:58 +0900

    Add files via upload

--- DAY5/1_friend.cpp
+++ DAY5/1_friend.cpp
@@ -1,10 +1,19 @@
 // 1_friend - 75 page
 
+class AAA
+{
+public:
+	void f()
+	{
+//		Bike b;
+//		b.gear = 10;
+	}
+};
+
 class Bike
 {
 	int gear;
 public:
-
 	// friend 함수 : 멤버 함수는 아니지만 private 멤버에 접근할수 있게 
 	//				 해달라는 의미.
 	// 1. 멤버함수로 하면 될텐데, 왜 멤버가 아닌 함수로 하나요
@@ -13,14 +22,16 @@ public:
 	//    => setter/getter를 만드는 것은 "어디서나 접근 가능" 하겠다는 의도.
 	//    => 아래 friend 는 "foo"만 접근 가능.
 	friend void foo();
+	friend class AAA; // AAA 의 모든 멤버 함수 에서 나의 private 접근 가능.
+	friend void AAA::f();
 };
 
-void foo()
+
+void foo() 
 {
 	Bike b;
 	b.gear = 10;
 }
-
 int main()
 {
 	foo();
